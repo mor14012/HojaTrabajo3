b1a6ed9a1dcaa898336cb72c13da6f01bb077937
Pablo Arriola <pabloiad@hotmail.com>
2015-08-06 19:57:20 -0600

    cambio

--- Beta0.3/sort.java
+++ Beta0.3/sort.java
@@ -71,20 +71,13 @@ public class sort{
 		return array;
 		}
 	
-	public static int[] sort(int[] old) {
+	public static num[] sort(num[] old) {
 	  
 	    for (int shift = Integer.SIZE - 1; shift > -1; shift--) {
-	   
 	        int[] tmp = new int[old.length];
-	 
 	        int j = 0;
-	 
-	    
 	        for (int i = 0; i < old.length; i++) {
-	          
 	            boolean move = old[i] << shift >= 0;
-	 
-	       
 	            if (shift == 0 ? !move : move) {
 	                tmp[j] = old[i];
 	                j++;
@@ -93,13 +86,10 @@ public class sort{
 	                old[i - j] = old[i];
 	            }
 	        }
-	 
-
 	        for (int i = j; i < tmp.length; i++) {
 	            tmp[i] = old[i - j];
 	        }
 	 
-	  
 	        old = tmp;
 	    }
 	 
